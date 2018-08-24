9430723b65f02eec3c23bede2247b00d477e6f7b
Michael Blazejewski <blazejewski.michael@gmail.com>
2018-08-24 15:28:44 +0200

    Nicer seeds

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,6 +1,6 @@
 class Building < ApplicationRecord
   validates :street, presence: true, length: { maximum: 100 }
-  validates :building_number, presence: true, length: { maximum: 4 }
+  validates :building_number, presence: true, length: { maximum: 5 }
   validates :city, presence: true, length: { maximum: 100 }
   VALID_PL_ZIP_CODE_REGEX = /[0-9]{2}-[0-9]{3}/
   validates :zip_code,
