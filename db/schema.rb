3537d08f0312de187da982b7d413229935678674
coadam <adam756@gmail.com>
2018-08-24 15:02:29 +0200

    configure .gitignore

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_08_24_101405) do
 ActiveRecord::Schema.define(version: 2018_08_24_115341) do
 
   # These are extensions that must be enabled in order to support this database
@@ -45,19 +43,11 @@ ActiveRecord::Schema.define(version: 2018_08_24_115341) do
     t.datetime "updated_at", null: false
     t.index ["estate_id"], name: "index_buildings_on_estate_id"
   end
-  
-    create_table "apartments", force: :cascade do |t|
-    t.integer "apartment_number"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end 
-  
+
   create_table "estates", force: :cascade do |t|
     t.string "name"
-    t.bigint "building_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["building_id"], name: "index_estates_on_building_id"
   end
 
   create_table "users", force: :cascade do |t|
