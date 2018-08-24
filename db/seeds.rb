686c405499dc7e21e14aac0e05de01a371062b45
coadam <adam756@gmail.com>
2018-08-24 13:56:07 +0200

    configure faker to pl locale

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Faker::Config.locale = 'pl'
+
 50.times do
   street = Faker::Address.street_name
   building_number = Faker::Address.building_number
