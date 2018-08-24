1a646b4442deff0dca37cca2133b1c8be7c883fe
Michael Blazejewski <blazejewski.michael@gmail.com>
2018-08-24 12:51:27 +0200

    Apartment model

---
+++ app/models/apartment.rb
@@ -0,0 +1,6 @@
+class Apartment < ApplicationRecord
+  validates :apartment_number, presence: true,
+            format: { with: /\A\d+\z/,
+                      message: "Integer only. No sign allowed." },
+            length: { in: 1..4 }
+end
