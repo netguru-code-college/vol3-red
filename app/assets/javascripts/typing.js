536bfbf21b0c8985dca45d51b7d5bd69023e6e91
Jakub Skoczylas <jakub.skoczylas@gmail.com>
2018-08-26 00:51:08 +0200

    I changed speed in typing.js

--- app/assets/javascripts/typing.js
+++ app/assets/javascripts/typing.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   console.log("Message");
   var i = 0;
-  var txt = "The best app for managing estate's community";
-  var speed = 100;
+  var txt = "The best app for managing estate's community...";
+  var speed = 110;
   typeWriter();
 
   function typeWriter() {
