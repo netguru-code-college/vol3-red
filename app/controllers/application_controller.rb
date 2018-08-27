229310b37935902f67bbb16651dc58ffa08d1ee2
Michael Blazejewski <blazejewski.michael@gmail.com>
2018-08-27 10:54:09 +0200

    Add Pry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
+  require 'pry'
   protected
-    def after_sign_in_path_for(resource)
-      stored_location_for(resource) ||
-        if resource.is_a?(User)
-          '/dashboard'
-        else
-          super
-        end
-    end
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) ||
+      if resource.is_a?(User)
+        '/dashboard'
+      else
+        super
+      end
+  end
 end
