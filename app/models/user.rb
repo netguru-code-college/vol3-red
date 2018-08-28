6ad526e81eee8d6ad8d033a45ff8ba04473768a8
Snejp <snejpik@gmail.com>
2018-08-28 10:30:47 +0200

    add Name Surname to users
    fix rspec

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+  validates :name, :surname, :email, presence: true
   has_many :apartment_users
   has_many :apartments, through: :apartment_users
 end
