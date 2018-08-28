9f7076f24fafcffd816a9d104967b4fee8b5f27c
Snejp <snejpik@gmail.com>
2018-08-28 12:56:19 +0200

    is admin methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,27 @@ class User < ApplicationRecord
   validates :name, :surname, :email, presence: true
   has_many :apartment_users
   has_many :apartments, through: :apartment_users
+
+  def admin?
+    is_admin = false
+    self.apartment_users.map do |apartment_user|
+      pp apartment_user
+      if apartment_user.status == 'admin' 
+        is_admin = true
+      end
+    end
+    is_admin
+  end
+
+  def admin=(admin)
+    is_admin = admin ? true : false
+    puts 'set admin'
+    self.apartment_users.map do |apartment_user|
+      if is_admin
+        apartment_user.status = 'admin'
+        apartment_user.save!
+      end
+    end
+  end
+
 end
