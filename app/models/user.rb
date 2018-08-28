1ba5ce71ecbeca744ab33a884fcac271bfcd653b
Snejp <snejpik@gmail.com>
2018-08-28 16:13:17 +0200

    carrier wave user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,13 @@ class User < ApplicationRecord
   validates :name, :surname, :email, presence: true
   has_many :apartment_users
   has_many :apartments, through: :apartment_users
+
+  mount_uploader :thumbnail, ThumbnailUploader
+  validates_integrity_of  :thumbnail
+  validates_processing_of :thumbnail
+ 
+  private
+    def avatar_size_validation
+      errors[:thumbnail] << "should be less than 500KB" if thumbnail.size > 0.5.megabytes
+    end
 end
