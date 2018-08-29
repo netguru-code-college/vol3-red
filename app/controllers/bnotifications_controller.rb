b99a23f92c60005408942518b63249d83b8d5fa2
coadam <adam756@gmail.com>
2018-08-29 14:03:31 +0200

    add bnotification

--- app/controllers/bnotifications_controller.rb
+++ app/controllers/bnotifications_controller.rb
@@ -4,4 +4,27 @@ class BnotificationsController < ApplicationController
     @bnotifications = Bnotification.all
   end
 
+  def create
+    @bnotification = Bnotification.new(bnotification_params)
+    if @bnotification.save
+      redirect_to bnotifications_path,
+                  notice: 'Notice was successfully created.'
+    else
+      redirect_to bnotifications_path,
+                  notice: 'Hello! it is meeeee! Your error!'
+    end
+  end
+
+  def new
+    @bnotification = Bnotification.new
+  end
+
+  def show
+    @bnotification = bnotification.find(params[:id])
+  end
+
+  private
+  def bnotification_params
+    params.require(:bnotification).permit(:building_id, :title, :body, :valid_up_to_date)
+  end
 end
