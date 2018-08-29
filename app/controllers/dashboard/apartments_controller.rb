50cc1f47cf38d9efd2243382e446987ff875f1d8
Jakub Skoczylas <jakub.skoczylas@gmail.com>
2018-08-29 09:56:35 +0200

    links fixed

--- app/controllers/dashboard/apartments_controller.rb
+++ app/controllers/dashboard/apartments_controller.rb
@@ -1,14 +1,14 @@
 class Dashboard::ApartmentsController < ApplicationController
   layout 'dashboard'
   before_action :authenticate_user!
-  before_action :find_estate, only: [:show, :edit, :update, :destroy]
+  before_action :find_estate, only: [:index, :show, :edit, :update, :destroy]
 
   def index
     @user = current_user
     @estates = Estate.all
     @buildings = Building.all
-    @building = @estates.buildings.find(params[:id])
-    @apartments = Apartment.all
+    @building = Building.find(params[:building_id])
+    @apartments = @building.apartments
 
   end
 
@@ -41,7 +41,7 @@ class Dashboard::ApartmentsController < ApplicationController
 
   private
   def find_estate
-    @estate = Estate.find(params[:id])
+    @estate = Estate.find(params[:estate_id])
   end
 
   def estate_params
