7c954b1e21288f6c09fbf4596ce8e2789a6d0443
coadam <adam756@gmail.com>
2018-08-29 14:34:35 +0200

    add bnotifications and enotifications

--- app/controllers/enotifications_controller.rb
+++ app/controllers/enotifications_controller.rb
@@ -1,7 +1,30 @@
 class EnotificationsController < ApplicationController
-
-
   def index
     @enotifications = Enotification.all
+  end
+
+  def create
+    @enotification = Enotification.new(enotification_params)
+    if @enotification.save
+      redirect_to enotifications_path,
+                  notice: 'Notice was successfully created.'
+    else
+      redirect_to enotifications_path,
+                  notice: 'Hello! it is meeeee! Your error!'
+    end
+  end
+
+  def new
+    @enotification = Enotification.new
+  end
+
+  def show
+    @enotification = enotification.find(params[:id])
+  end
+
+  private
+
+  def enotification_params
+    params.require(:enotification).permit(:estate_id, :title, :body, :valid_up_to_date)
   end
 end
